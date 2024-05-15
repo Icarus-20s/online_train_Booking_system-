ea43787a9002bd550e3d3157c02fb9562896a941
Icarus-20s <ayushask.2002@gmail.com>
2024-05-15 23:25:32 +0545

    add:JWT TOken

--- reservationApp/views.py
+++ reservationApp/views.py
@@ -1,27 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.models import User,auth
+from django.contrib.auth.models import User, auth
 from rest_framework.decorators import api_view
 from .serializer import UserLoginSerializer, UserSerializer
 from django.contrib.auth.hashers import make_password, check_password
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # Create your views here.
 
-def homePage(request):
-    return render(request , 'home.html')
 
-@api_view(["POST" , "GET"])
+def homePage(request):
+    return render(request, "home.html")
+
+
+@api_view(["POST", "GET"])
 def reservation(request):
     serializer = UserSerializer(data=request.data)
     if not serializer.is_valid():
-        return render(request , 'register.html')
+        return render(request, "register.html")
     serializer.save()
-    return render(request , 'login.html')
-@api_view(["POST" , "GET"])
+    return render(request, "login.html")
+
+
+@api_view(["POST", "GET"])
 def login(request):
-    if request.method=="GET":
-         return render(request , 'login.html')
+    if request.method == "GET":
+        return render(request, "login.html")
     print(request.__dict__)
     serializer = UserLoginSerializer(data=request.data)
     if not serializer.is_valid():
@@ -31,10 +36,9 @@ def login(request):
     user = User.objects.filter(email=email).first()
     if not user:
         return HttpResponse("user not found!!")
-    
+
     if not check_password(password, user.password):
-                return HttpResponse("Invalid credentials")
-    return HttpResponse("Logged in")
+        return HttpResponse("Invalid credentials")
+    token = RefreshToken.for_user(user)
 
-
-
+    return HttpResponse(str(token))
